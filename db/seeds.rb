1c0ae13b4dca62615e87f7af3d13ffda4246e520
Lisztomaniaa <lizontik@gmail.com>
2018-08-13 16:06:59 +0300

    added set of categories to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+categorys = Category.create ([{name: 'Bikes'},{name: 'Beach rents'},{name: 'Camps & Tours'},{name: 'Winter rents'}])
