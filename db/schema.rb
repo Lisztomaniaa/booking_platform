79f4739e2d2c5d59e01fc5d8ba36743d3ee4b8cb
Lisztomaniaa <lizontik@gmail.com>
2018-08-30 13:29:09 +0300

    fixed migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_21_162309) do
+ActiveRecord::Schema.define(version: 2018_08_28_170113) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2018_08_21_162309) do
     t.integer "renter_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "item_id"
+    t.index ["item_id"], name: "index_bookings_on_item_id"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -79,6 +81,7 @@ ActiveRecord::Schema.define(version: 2018_08_21_162309) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "items"
   add_foreign_key "items", "categories"
   add_foreign_key "items", "cities"
 end
